4f5112c9d41d7d6a222d49964be34ca67ffc8fc6
Torres Mateu, Alejandro <Alejandro.TorresMateu@adidas-group.com>
2016-06-20 16:57:43 +0200

    moved files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname));////
+app.use(express.static(__dirname + '/public'));////
 
 app.listen(app.get('port'), function() {
 });
